27b106e5cc3de6d61ce0e8f388c177294fb6823d
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-04-19 14:09:53 -0300

    variaveis

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,82 @@
+// Interpretação
+
+// 1.
+// 10
+// 10 5
+
+// 2.
+// 10 10 10
+
+
+// Escrita
+
+// 1.
+
+// let nome  
+// // não da pra criar const sem valor, dei um valor qualquer
+// let idade;
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// nome = prompt("qual seu nome?")
+// // Não da para alterar varialvel const - troquei para let e deixei undefined
+// idade = prompt("qual sua idade?")
+
+// console.log(nome, idade)
+
+// console.log("Olá " + nome + " você tem " + idade + " anos")
+
+// 2.
+
+// let cidade, pets, quantosPets, serieFilme, batata
+
+// cidade = prompt("Que cidade você mora?")
+// console.log("Que cidade você mora?")
+// console.log("Sua Resposta: " + cidade)
+
+// pets = prompt("você tem animais de estimação?")
+// console.log("Você tem animais de estimação?")
+// console.log("Sua Resposta: " + pets)
+
+// quantosPets = prompt("Se sim, quantos? (colocar 0 se não)")
+// console.log("se sim, quantos?")
+// console.log("Sua Resposta: " + quantosPets)
+
+// serieFilme = prompt("qual seu/sua filme/serie favorito(a)?")
+// console.log("qual seu/sua filme/serie favotito(a)?")
+// console.log("Sua Resposta: " + serieFilme )
+
+// batata = prompt("Gosta de batata?")
+// console.log("Gosta de batata?")
+// console.log("Sua Resposta: " + batata)
+
+// 3.
+
+// const comida = ["castanha de caju", "chocolate", "gergelim", "pizza", "sushi-veg"]
+
+// console.log (comida)
+
+// console.log ("essas são minhas comidas preferidas:")
+// console.log (comida[0])
+// console.log (comida[1])
+// console.log (comida[2])
+// console.log (comida[3])
+// console.log (comida[4])
+
+// comida[1] = prompt("qual sua comida favorita?")
+// console.log (comida[0])
+// console.log (comida[1])
+// console.log (comida[2])
+// console.log (comida[3])
+// console.log (comida[4])
+
+// 4.
+
+// const perguntas = [prompt("gosta de batata? "), prompt("gosta de gatos?"), prompt("tem filhos? ") ]
+
+// const respostas = [true, true, false]
+
+// console.log ("gosta de batata? " + respostas[0])
+// console.log ("gosta de gatos? " + respostas[1])
+// console.log ("tem filhos? " + respostas[2])
