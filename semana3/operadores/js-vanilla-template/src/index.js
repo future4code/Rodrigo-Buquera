b709307f7a567565bf342ac0af519409506e3712
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-04-20 15:04:02 -0300

    operadores

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,159 @@
+// Interpretação 
+// 1.
+
+// a. false
+// b. false
+// c. true
+// d. boolean
+
+2.
+
+// a. undefined 
+// b. null
+// c. 11
+// d. 3
+// e. 3, 19 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+
+
+// Escrita
+// 1.
+
+// let idade = prompt("qual a sua idade")
+// let idadeAmigo = prompt("qual a idade do seu melhor amigo?")
+
+// Number(idade)
+// Number(idadeAmigo)
+
+// console.log("Sua idade é amior que a do seu melhor amigo? ", idade>=idadeAmigo) 
+
+// console.log (idade - idadeAmigo)
+
+// 2.
+
+// let numero = prompt("digite um numero par")
+
+// Number(numero)
+
+// console.log(numero%2) 
+
+// o resto da divisão de números pares é sempre 1, enquanto de números impares é sempre 1"
+
+// 3.
+
+// let listaDeTarefas = []
+
+// listaDeTarefas[0]  = prompt("Qual a sua primeira tarefa do dia?")
+// listaDeTarefas[1]  = prompt("Qual a sua segunda tarefa do dia?")
+// listaDeTarefas[2]  = prompt("Qual a sua terceira tarefa do dia?")
+
+// // console.log(listaDeTarefas)
+
+// let indice = prompt("qual dessas tarefas você já fez? de 0 a 2")
+// Number(indice)
+
+// let removido
+// removido = listaDeTarefas.splice(indice,1)
+
+// console.log(listaDeTarefas)
+
+// 4.
+
+// let nomeDoUsuario = prompt("qual seu nome?")
+// let emailDoUsuario = prompt("qual seu email?")
+
+// console.log(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeDoUsuario}`)
+
+// Desafios
+
+// 1.
+
+// let grauCelsius
+// let kelvin
+// let grauFahrenheit
+
+//a
+// grauFahrenheit = 77
+
+//b
+// grauCelsius = 80
+
+//c
+// grauCelsius = 30
+
+//d
+// grauCelsius = prompt("insira a temperatura em Celsius")
+// Number(grauCelsius)
+
+// grauFahrenheit = grauCelsius*9/5 +32
+// kelvin = (grauFahrenheit -32)*5/9 +273.15
+
+// //a
+// // console.log(kelvin +"K")
+
+// //b
+// // console.log(grauFahrenheit +"°F")
+
+// //c
+// // console.log(grauFahrenheit +"°F")
+// // console.log(kelvin +"K")
+
+// //d
+// console.log(grauFahrenheit +"°F")
+// console.log(kelvin +"K")
+
+
+// 2.
+
+// const custoQwh =  0.05
+// let consumo = prompt("qual seu consumo em QWH?")
+// Number(consumo)
+
+// console.log("Você deve pagar R$", consumo*custoQwh)
+
+// console.log("Com desconto você deve pagar R$", consumo*custoQwh*0.85)
+
+// 3.
+//a.
+
+// let pesoLibra = 20
+// let pesoQuilo=pesoLibra/2.4046
+
+// console.log(`20lb equivalem a ${pesoQuilo} kg`)
+
+//b
+// let pesoOnca= 10.5
+// let pesoQuilo=pesoOnca/35.274
+
+// console.log(`20lb equivalem a ${pesoQuilo} kg`)
+
+//c.
+// let milhas=100
+// let metros=milhas/0.00062137
+
+// console.log(`100mi equivalem a ${metros} m`)
+
+//d.
+// let pes=50
+// let metros=pes/3.2808
+
+// console.log(`50ft equivalem a ${metros} m`)
+
+//e.
+// let galao=103.56
+// let litros=galao/0.26417
+
+// console.log(`103.56gal equivalem a ${litros} l`)
+
+//f.
+// let xicaras=450
+// let litros=xicaras*6/25
+
+// console.log(`450 xic equivalem a ${litros} l`)
+
+//g.
+
+// let pesoLibra = prompt("entre com um peso em libra")
+// Number(pesoLibra)
+// let pesoQuilo=pesoLibra/2.4046
+
+// console.log(`100lb equivalem a ${pesoQuilo} kg`)
