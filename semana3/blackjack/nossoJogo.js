3d6182e480a6812df37064bfd88e3107b021d978
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-04-23 16:36:17 -0300

    projeto blackjack

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,41 @@
+
+// EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+
+// let carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+
+// console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+// console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+
+// let cartasUsuario = []
+// let cartasComputador = []
+// let somaUsuario
+// let somaComputador
+
+// console.log("Bem vindo ao jogo de Blackjack")
+
+// if (confirm("Quer iniciar uma rodada?")) {
+//    for (i = 0; i < 2; i++) {
+//       carta = comprarCarta()
+//       cartasUsuario.push(carta)
+//       carta = comprarCarta()
+//       cartasComputador.push(carta)
+//    }
+//    somaUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor
+//    somaComputador = cartasComputador[0].valor + cartasComputador[1].valor
+
+//    console.log(`Usuário - cartas ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} pontuação ${somaUsuario}`)
+//    console.log(`Computador - cartas ${cartasComputador[0].texto} ${cartasComputador[1].texto} pontuação ${somaComputador}`)
+
+//    if (somaComputador === somaUsuario) {
+//       console.log("empate")
+//    } else if (somaComputador > somaUsuario) {
+//       console.log("o computador venceu")
+//    } else {
+//       console.log("o usuário venceu")
+//    }
+
+// } else {
+//    console.log("O jogo acabou")
+// }
+
+
