7de5415603620618c28718311adfbdc572871964
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-04-27 16:35:52 -0300

    DOM (desafio 4 incompleto)

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,11 +1,18 @@
+// const diasAgenda = ["domingo","segunda","terca","quarta", "quinta", "sexta", "sabado"]
 
+// for (dia of diasAgenda){
+//     for (let num=0; num <24; num++){
+//         horarioAgenda = document.getElementById(`${dia}`)
+//         horarioAgenda.innerHTML += `<ul  class="${num}, estilo-hora"> ${num}h </ul>`
+//     }
+// }
+// não deu tempo de finalizar o desafio 4
 
 function adicionarTarefa() {
 
     const diasSemana = document.getElementById("dias-semana")
     const tarefa = document.getElementById("tarefa")
     const tarefaDia = document.getElementById(`${diasSemana.value}`)
-   
     
     if (tarefa.value !== "") {        
         tarefaDia.innerHTML += `<ul onclick="clicar(this)"> ${tarefa.value} </ul>`        
