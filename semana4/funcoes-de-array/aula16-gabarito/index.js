f042a685c5ebd2b7a4895cdeae8dcc730b29d5bb
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-04-29 15:44:38 -0300

    callback (sem desafio)

---
+++ semana4/funcoes-de-array/aula16-gabarito/index.js
@@ -0,0 +1,142 @@
+let arrDespesas = []
+imprimirDespesas(arrDespesas)
+imprimirExtrato()
+
+
+// PRIMEIRO
+function imprimirDespesas(despesas) {
+    let divDespesas = document.getElementById('despesas')
+    divDespesas.innerHTML = '<p><u>Despesas Detalhadas</u></p>'
+
+    // AQUI VEM A IMPLEMENTAÇÃO
+
+    despesas.forEach((despesaImpressa) => {
+        divDespesas.innerHTML += `<div> Valor:R$ ${despesaImpressa.valor}| tipo: ${despesaImpressa.tipo} | descrição: ${despesaImpressa.descricao} </div>`
+    })
+
+    // FIM DA IMPLEMENTAÇÃO
+}
+
+
+// SEGUNDO 
+function imprimirExtrato() {
+    let divExtrato = document.getElementById('extrato')
+    let gastoTotal = 0
+    let gastoAlimentacao = 0
+    let gastoUtilidades = 0
+    let gastoViagem = 0
+
+
+    // AQUI VEM A IMPLEMENTAÇÃO
+
+    arrDespesas.forEach((itemDespesa) => {
+        if (itemDespesa.tipo === "alimentação") {
+            gastoAlimentacao += itemDespesa.valor
+        } else if (itemDespesa.tipo === "utilidades") {
+            gastoUtilidades += itemDespesa.valor
+        } else if (itemDespesa.tipo === "viagem") {
+            gastoViagem += itemDespesa.valor
+        }
+    })
+
+    gastoTotal = gastoUtilidades + gastoViagem + gastoAlimentacao
+
+    // FIM DA IMPLEMANTAÇÃO
+
+    divExtrato.innerHTML = `<p>Extrato: Gasto Total: R$${gastoTotal} | Alimentação: R$${gastoAlimentacao} | 
+                                        Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`
+}
+
+
+function limparFiltros() {
+    document.getElementById('tipoFiltro').value = ""
+    document.getElementById('valorFiltroMin').value = ""
+    document.getElementById('valorFiltroMax').value = ""
+}
+
+
+
+function adicionarDespesa() {
+    let valorCdt = document.getElementById('valorCadastro')
+    let tipoCtd = document.getElementById('tipoCadastro')
+    let descricaoCtd = document.getElementById('descricaoCadastro')
+
+    if (validarValor(valorCdt) && validarTipo(tipoCtd) && validarDescricao(descricaoCtd)) {
+        let novaDespesa = {
+            valor: Number(valorCdt.value),
+            tipo: tipoCtd.value,
+            descricao: descricaoCtd.value,
+        }
+
+        arrDespesas.push(novaDespesa)
+
+        valorCdt.value = ""
+        tipoCtd.value = ""
+        descricaoCtd.value = ""
+
+
+        limparFiltros()
+        imprimirDespesas(arrDespesas)
+        imprimirExtrato()
+    } else {
+        alert("`Faltou algum valor ou algum valor é um número negativo`")
+    }
+}
+
+
+
+// TERCEIRO
+function filtrarDespesas() {
+    let tipoFiltro = document.getElementById('tipoFiltro').value
+    let valorMin = Number(document.getElementById('valorFiltroMin').value)
+    let valorMax = Number(document.getElementById('valorFiltroMax').value)
+
+    // AQUI NESSA VARIÁVEL VEM A IMPLEMENTAÇÃO
+
+    // if ( valorMin ===""){
+    //     valorMin = 0
+    // }
+    // if (valorMax ===""){
+    //     valorMax = Infinity
+    // }
+    // NÃO DEU CERTO
+
+    let despesasFiltradas = arrDespesas.filter((itemFiltrado) => {
+        if (itemFiltrado.valor >= valorMin && itemFiltrado.valor <= valorMax && itemFiltrado.tipo === tipoFiltro) {          
+            return true
+        }
+    })
+    
+    imprimirDespesas(despesasFiltradas)
+}
+
+
+
+
+
+
+
+// FunÇoes que fazem validaÇoes dos inputs de criaÇao de despesas 
+
+// NÃO SE PREOCUPEM EM ENTENDER ESSAS FUNÇÕES
+
+function validarValor(valor) {
+    if (valor.value.length > 0 && parseInt(valor.value) > 0) {
+        return true
+    }
+    return false
+}
+
+function validarTipo(tipo) {
+    if (tipo.value !== "") {
+        return true
+    }
+    return false
+}
+
+function validarDescricao(texto) {
+    if (texto.value.replace(/ /g, "").length !== 0) {
+        return true
+    }
+    return false
+}
