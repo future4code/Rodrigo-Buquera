c23f7db7e8589bca3cc2f41ef80dcb7dca47d8c5
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-05-05 15:23:56 -0300

    desafio 3 - final

--- semana5/insta4/src/components/SecaoCompartilhado/SecaoCompartilhado.js
+++ semana5/insta4/src/components/SecaoCompartilhado/SecaoCompartilhado.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const CommentCompartilhado = styled.div`
@@ -13,28 +13,47 @@ const Icones = styled.img`
 	max-height: 1.3em;
 `
 
-export class SecaoCompartilhado extends Component {
-	state = {
-		valorInput: ""
-	}
+const InputCompartilhado = styled.input`
+    width: 100%;
+  
+`
 
-    onClickInstagram =() => {
+export class SecaoCompartilhado extends Component {
+    state = {
+        valorInput: ""
+    }
+
+    onClickInstagram = () => {
         console.log("você compartilhou no Instagram")
     }
 
-    onClickFacebook =() => {
+    onClickFacebook = () => {
         console.log("você compartilhou no facebook")
     }
 
-    onClickTwiter =() => {
+    onClickTwiter = () => {
         console.log("você compartilhou no twiter")
     }
 
-	render() {
-		return <CommentCompartilhado>
-			<Icones alt={'Icone'} src={"https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"} onClick={this.onClickInstagram} />
+    onChangeSharedComent = (event) => {
+        this.setState({valorInput: event.target.value})
+		console.log(this.state.valorInput)
+    }
+
+    render() {
+        return <CommentCompartilhado>
+            <Icones alt={'Icone'} src={"https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"} onClick={this.onClickInstagram} />
             <Icones alt={'Icone'} src={"https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"} onClick={this.onClickFacebook} />
-            <Icones alt={'Icone'} src={"https://upload.wikimedia.org/wikipedia/pt/3/3d/Twitter_logo_2012.svg"} onClick={this.onClickTwiter} />            				
-		</CommentCompartilhado>
-	}
+            <Icones alt={'Icone'} src={"https://upload.wikimedia.org/wikipedia/pt/3/3d/Twitter_logo_2012.svg"} onClick={this.onClickTwiter} />
+
+            <InputCompartilhado
+                placeholder={'Comentário'}
+                value={this.state.valorInput}
+                onChange={this.onChangeSharedComent}
+            />
+            <button onClick={this.props.aoEnviar} >Enviar</button>
+
+
+        </CommentCompartilhado>
+    }
 }
