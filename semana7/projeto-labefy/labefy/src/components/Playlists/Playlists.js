ed8e0ef832b9bbf7c90d502723b004e7cf01f43b
rodrigo-brezolin-buquera <rodrigobbuquera@gmail.com>
2021-05-21 16:33:37 -0300

    labefy

---
+++ semana7/projeto-labefy/labefy/src/components/Playlists/Playlists.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+
+export default class Playlists extends React.Component { 
+
+render(){
+    return {
+        
+    }
+}
+}
